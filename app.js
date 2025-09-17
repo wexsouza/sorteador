a95582d1c21bfcfa859ad508d79094c88d643b20
wexsouza <wesley.dnx@gmail.com>
2025-09-17 15:05:41 -0300

    javascript coloquei nao ter repeticao de numero

--- app.js
+++ app.js
@@ -8,6 +8,11 @@ let numero;
 
 for (let i = 0; i < quantidade; i++) {
     numero = obterNumeroAleatorio(de, ate);
+ 
+    while (sorteados.includes(numero)){
+        numero = obterNumeroAleatorio(de, ate);
+    }
+
     sorteados.push(numero);
 }
 
